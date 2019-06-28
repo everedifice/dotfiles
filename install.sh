3dfbb157b02666edae4f2dca42637e45813d85aa
johnny-mh <romz1212@gmail.com>
2019-06-28 23:42:51 +0900

    인스톨

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# install tmux
-brew install tmux --HEAD
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# install iterm2-nightly
-brew tap caskroom/versions
-brew cask install iterm2-nightly
-
-ln -s .tmux.conf ~/.tmux.conf
-ln -s .vimrc ~/.vimrc
+ln -s -f .tmux.conf ~/.tmux.conf
+ln -s -f .vimrc ~/.vimrc
